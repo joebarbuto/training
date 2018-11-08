79d0cfc6e3c03a61a3cabf733c44b0c2b73b8135
jbarbuto <nygfan992@gmail.com>
2018-11-08 15:31:48 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+     @base_title = "Lego Training App"
+   end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get jobs" do
     get static_pages_jobs_url
     assert_response :success
+    assert_select "title", "Jobs | #{@base_title}"
   end
 
   test "should get fun" do
     get static_pages_fun_url
     assert_response :success
+    assert_select "title", "Fun | #{@base_title}"
   end
 
   test "should get living" do
     get static_pages_living_url
     assert_response :success
+    assert_select "title", "Living | #{@base_title}"
   end
 
 end
